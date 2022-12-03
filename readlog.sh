31949ced328b7dbbd2f23383cd3714c77b0e995c
Oleh Dovhanych <olegdovg777@gmail.com>
2022-12-03 11:27:12 +0200

    6. What search bots have accessed the site? (UA + IP)

--- readlog.sh
+++ readlog.sh
@@ -7,7 +7,7 @@ function request_per_day() {
     while read line; do
         day=$(echo "$line" | sed 's/.*\[//g;s/].*//g;s/:.*//g')
         if [[ -v day_array[$day] ]]; then
-           day_array[$day]=$((day_array[$day]+1))
+            day_array[$day]=$((day_array[$day]+1))
         else
             day_array[$day]=1
         fi
@@ -15,6 +15,7 @@ function request_per_day() {
 
     for day in ${!day_array[@]}; do echo ${day_array[$day]} $day; done | sort -rn | head -1
 }
+
 
 function request_per_ip() {
     declare -A ip_array
@@ -30,13 +31,34 @@ function request_per_ip() {
     for ip in ${!ip_array[@]}; do echo ${ip_array[$ip]} $ip; done | sort -rn | head -1
 }
 
+function request_per_time() {
+    declare -A time_array
+    while read line; do
+        time=$(echo $line | awk '{print $4}'| cut -c 14-21)
+        if [[ -v time_array[$time] ]]; then
+            time_array[$time]=$((time_array[$time]+1))
+        else
+            time_array[$time]=1
+        fi
+    done < $LOG_FILE
 
+    for time in ${!time_array[@]}; do echo ${time_array[$time]} $time; done | sort -rn | head -1
+}
 
+function request_per_bot() {
+
+    while read line; do
+        bot_ip="$(echo $line | awk '{print $1}') - $(echo $line | cut -d\" -f6)"
+        echo "$bot_ip" >> 1.log
+    done < $LOG_FILE
+
+    
+    cat 1.log | grep bot | sort | uniq 
+    rm -f 1.log
+}
 
 echo "1. From which ip were the most requests?"
  request_per_ip
-echo ""
- cat $1 | awk '{print \$1}' | uniq -c | sort -rn | head -1
 echo ""
 echo "2. What is the most requested page?"
  cat $1 |awk '($9 ~ /200/)' | awk '{print $9,$7}' | uniq -c | sort -rn | head -1
@@ -48,7 +70,7 @@ echo "4. What non-existent pages were clients referred to?"
  cat $1 |awk '($9 ~ /403/)' | awk '{print $9,$7}' | uniq -c | sort -rn | head -1
 echo ""
 echo "5. What time did site get the most requests?"
- request_per_time
+request_per_time
 echo ""
 echo "6. What search bots have accessed the site? (UA + IP)"
-
+request_per_bot
